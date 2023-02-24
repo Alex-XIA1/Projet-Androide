c3cfb2de7400093a8295308ab0040b7b6da45e8a
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2023-02-24 13:08:50 +0100

    Ajout d'un classifier raccourci possible ou impossible

--- Logiciel_afinir_tme/Model1.py
+++ Logiciel_afinir_tme/Model1.py
@@ -8,11 +8,12 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import normalize
-
+from scipy.stats import itemfreq
 import matplotlib.pyplot as plt
 import time
 import sklearn
 import warnings
+import PlayerBasic
 
 
 def getData():
@@ -76,53 +77,87 @@ class MLPtrick():
         
         return self.model.predict_proba(newX)
 
-def getNo(X,Y):
-    n,d = X.shape
-    c = d//2
-    E = np.array(X[:,:c]).astype("<U22")
-    print(E.shape)
-    etatInital = np.unique(E, axis=0)
-    L = []
-    i = 0
-    for e in etatInital:
-        etatFinal = X[np.where(e == X[:,:c], 0, 1)][:,c:]
-        print("Before ", etatFinal.shape)
-        etatFinal = np.unique(etatFinal.astype("<U22"), axis=0)
-        print(etatFinal.shape)
+# Add data
+# rows, impossible transition with label "No"
+def getNo(X,Y, max_n):
+    c = X.shape[1]//2
+    X = X.astype(int)
+    newX = X[:2000]
+    ratio = 0.5
+    coupe_n = int(len(newX)*ratio)
+    etatInital = np.unique(newX[:,:c], axis=0)[:coupe_n]
+    L = [] # list of added rows
+    i = 0 # number of added rows
+    for e in etatInital: 
+        # indices des etats impossibles depuis l'état e
+        indices = np.where(e != X[:,:c])[0][:10]
+        if len(indices) == 0: continue
+        # On supprime les duplicatas
+        etatFinal = np.unique(X[:,c:][indices], axis=0)
+        # On ajoutes les lignes
         for e1 in etatFinal:
             L.append(flatten_list([e, e1]))
             i+=1
+        if i>=max_n:
+            break
     L = np.array(L)
-    print("L", L.shape)
-    print("X; ", X.shape)
-    print(L)
-    newX = np.concatenate((X, L), axis = 0)
-    newY =  np.concatenate((Y, ["No"]*i), axis = 0)
-    return newX, newY
+    return np.concatenate((X, L), axis = 0), np.concatenate((Y, ["No"]*i), axis = 0)
     
 def flatten_list(_2d_list):
     flat_list = []
     # Iterate through the outer list
     for element in _2d_list:
-        if type(element) is list or type(element) is tuple:
+        if type(element) is list or type(element) is tuple or type(element) is np.ndarray:
             # If the element is of type list, iterate through the sublist
             flat_list.extend(flatten_list(element))
         else:
             flat_list.append(element)
     return flat_list    
         
+def show_stats(data):
+    V,C = np.unique(data, return_counts = True)
+    print("========= Stats ==========")
+    print(" Value  |  Counts")
+    for i in range(len(C)):
+        print(V[i], '  -  ', C[i])
+    print("==========================")
+
+warnings.simplefilter('ignore')
 
 def test():
-    warnings.simplefilter('ignore')
     X, Y = getData()  
-    print("Taille X: ", X.shape)
-    #X,Y =  getNo(X,Y)
+    X,Y = getNo(X,Y, 500)
+    show_stats(Y)
     n, d  = X.shape
 
-    #print(X,Y)
-    #X = normalize(X, axis = 0, norm='l1')
     # Classifier
-    cf = MLPtrick()#Tree()
+    cf = MLPtrick()
+    cf.fit(X,Y)
+    prediction = cf.predict(X)
+    print("\nscore: ", cf.score(X,Y))
+    for y in np.unique(Y):
+        indices = Y == y
+        print("Y: ", y, " - ", cf.score(X[indices],Y[indices]))
+        v,c = np.unique(prediction[indices][prediction[indices]!=y], return_counts=True)
+        for i in range(len(v)):
+            print("\t",v[i], " - ", c[i])
+        print()
+    return cf
+
+def testData(X,Y):
+    avant = len(X)
+    assert len(np.unique(X, axis=0)) == avant
+
+
+def testRaccourci():
+    X, Y = getData()  
+    Y = np.array(["Yes"]*len(Y))
+    X,Y = getNo(X,Y, len(X))
+    testData(X,Y)
+    show_stats(Y)
+    
+    # Classifier
+    cf = MLPtrick()
     cf.fit(X,Y)
     prediction = cf.predict(X)
     print("\nscore: ", cf.score(X,Y))
@@ -138,6 +173,7 @@ def test():
 
 
 
+
 if __name__=="__main__":
-    test()
+    testRaccourci()
 
