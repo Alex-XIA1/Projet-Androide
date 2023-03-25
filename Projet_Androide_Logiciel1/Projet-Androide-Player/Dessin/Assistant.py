e854ea21e9b369b8662f034e5a3d196ad2a0d516
A_X <78434551+Alex-XIA1@users.noreply.github.com>
2023-03-25 08:23:37 +0100

    Add files via upload

---
+++ Projet_Androide_Logiciel1/Projet-Androide-Player/Dessin/Assistant.py
@@ -0,0 +1,65 @@
+import csv
+
+
+def flatten_list(_2d_list):
+    flat_list = []
+    # Iterate through the outer list
+    for element in _2d_list:
+        if type(element) is list or type(element) is tuple:
+            # If the element is of type list, iterate through the sublist
+            flat_list.extend(flatten_list(element))
+        else:
+            flat_list.append(element)
+    return flat_list    
+
+class Assistant(): 
+    def __init__(self, model, app,zone) : 
+        self.model = model
+        self.app = app
+        self.app.logger.Lobserver.append(self)
+        self.zone = zone
+        self.cmd = []
+        self.logs =[]
+
+    def update(self, etatDep, etatFin, commande):
+        """if len(self.logs)>0:
+            # etat courant
+            end = etatFin
+            # etat passee ( n-2 )
+            start = self.logs.pop(0)
+            # cree la donnee a predire
+            x = flatten_list([start,end])
+            sortie = self.model.predict(x)
+
+            # Si true on dit a l'utilisateur d'utiliser la commande en sortie
+            if self.compare(sortie,self.cmd):
+                print("Utiliser la commande suivante:", sortie)
+                self.cmd.append(sortie)
+        self.logs.append(etatDep)
+        print("YES")"""
+        x =  [flatten_list([etatDep,etatFin])]
+        sortie = self.model.predict(x)
+        if self.compare(sortie,commande):
+            #print("PASSED ",self.cmd, commande)
+            # Potentiellement ne pas spammer l'utilisateur apres l'avoir fait decouvert la commande ?
+            if sortie not in self.cmd:
+                #print("Utiliser la commande suivante:", sortie)
+                texte = "On vous propose la commande suivante car il faut bourrer son pantatlon: "+sortie[0]
+                self.zone.setPlainText(texte)
+                self.cmd.append(sortie)
+
+    
+
+    # On va comparer la sortie du modele avec les commandes de l'utilisateur
+    def compare(self, sortie, logs):
+        if sortie == logs:
+            return False
+        # sortie a definir : label de type chiffre ou retranscrit vers un raccourci
+        return True
+    
+
+
+
+
+
+    
---
+++ Projet_Androide_Logiciel1/Projet-Androide-Player/Dessin/SimpleCanvas.py
@@ -0,0 +1,145 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import numpy as np
+
+
+class Canvas(QWidget):
+    def __init__(self, parent = None):
+        print("class Canvas")
+        super(Canvas,self).__init__()
+
+        self.parent = parent
+
+        self.setMinimumSize(300,300)
+        self.setMouseTracking(True)
+        self.cursorPos = None
+        self.pStart = None
+
+        # attributs d'affichage
+        self.bkcolor = QColor(Qt.blue)
+        self.width = 1
+        self.painterTranslation = QPoint(0,0)
+
+        # attributs memoire
+        self.rect = QRect(100,100, 100, 100)
+        self.color = None
+        self.form = None
+        
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        if self.color != None:
+            if self.color == "red":
+                painter.setBrush(Qt.red)
+            else: painter.setBrush(Qt.blue)
+        if self.form != None:
+            getattr(painter, self.form)(self.rect)
+
+    def reset(self):
+        self.color = None
+        self.form = None
+        self.update()
+
+
+    def set_color(self, color):
+        print("set color")
+        if self.mode == 'select' and self.selected!=None:
+            if self.color!=color:
+                self.color = color
+                self.update()
+        else:
+            self.bkcolor = color
+
+    @pyqtSlot()
+    def setTool(self,tool):
+        if tool == "rectangle":
+            self.form = "drawRect"
+        else:
+            self.form = "drawEllipse"
+
+
+################################################################
+#Fonctions pour le test du players
+    def etat(self):
+        a = self.form
+        b = self.color
+        if a == None:
+            a = "Rien"
+        if b == None:
+            b = "Rien"
+        return (a, b)
+
+    def dessinRedRect(self) : 
+        etat1 = self.etat()
+        self.form = "drawRect"
+        self.color = "red"
+
+        etat2 = self.etat()
+        self.parent.logger.addRow(etat1, etat2, "dessinRedRect")
+        self.update()
+
+    def dessinRect(self) : 
+        etat1 = self.etat()
+        self.form = "drawRect"
+        self.color = "Rien"
+
+        etat2 = self.etat()
+        self.parent.logger.addRow(etat1, etat2, "dessinRect")
+        self.update()
+
+    def dessinEllipse(self) : 
+        etat1 = self.etat()
+        self.form = "drawEllipse"
+        self.color = "Rien"
+        etat2 = self.etat()
+        self.parent.logger.addRow(etat1, etat2, "dessinEllipse")
+        self.update()
+
+    def dessinBlueRect(self) : 
+        etat1 = self.etat()
+        self.form = "drawRect"
+        self.color = "blue"
+
+        etat2 = self.etat()
+        self.parent.logger.addRow(etat1, etat2, "dessinBlueRect")
+        self.update()
+
+    def dessinRedEllipse(self) : 
+        etat1 = self.etat()
+        self.form = "drawEllipse"
+        self.color = "red"
+
+        etat2 = self.etat()
+        self.parent.logger.addRow(etat1, etat2, "dessinRedEllipse")
+        self.update()
+
+    def dessinBlueEllipse(self) : 
+        etat1 = self.etat()
+        self.form = "drawEllipse"
+        self.color = "blue"
+
+        etat2 = self.etat()
+        self.parent.logger.addRow(etat1, etat2, "dessinBlueEllipse")
+        self.update()
+        
+    
+    def setRed(self):
+        if self.color == "red":
+            return
+        
+        etat1 = self.etat()
+        self.color = "red"
+        etat2 = self.etat()
+        self.parent.logger.addRow(etat1, etat2, "setRed")
+        self.update()
+
+    def setBlue(self):
+        if self.color == "blue":
+            return 
+        etat1 = self.etat()
+        self.color = "blue"
+        etat2 = self.etat()
+        self.parent.logger.addRow(etat1, etat2, "setBlue")
+        self.update()
+    
+
